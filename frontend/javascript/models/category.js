d67065716df28f3e061ad828cc3fbc973151b219
Bhavani <bhavanivamshee@gmail.com>
2021-06-30 21:21:12 -0500

    adds render for categories

--- frontend/javascript/models/category.js
+++ frontend/javascript/models/category.js
@@ -1,3 +1,14 @@
 class Category{
-    
+    constructor(id, category_name){
+        this.id = id
+        this.category_name = category_name
+    }
+
+    renderCategory(){
+        const categoryHolder = document.getElementById("movies-categories-list")
+        const categoryContainer = document.createElement('div')
+        categoryContainer.dataset.id = this.id
+        categoryContainer.id = this.id
+        categoryContainer.classList.add = "movie-categories-add"
+    }
 }
