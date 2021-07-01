06925156835bb996b72be43ddeff0a3ee092753c
Bhavani <bhavanivamshee@gmail.com>
2021-06-30 21:11:03 -0500

    adds new category

--- frontend/javascript/services/API.js
+++ frontend/javascript/services/API.js
@@ -2,6 +2,11 @@ class API {
     static addMoviesCategories(){
         fetch("http://localhost:3000/categories")
         .then(resp => resp.json())
-        .then(console.log)
+        .then(categories => {
+            categories.forEach(category => {
+                const{id, category_name} = category
+                new Category(id, category_name)
+            })
+        })
     }
 }
