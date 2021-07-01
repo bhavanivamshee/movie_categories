b2a306d7a270d44bb1d8ad2ad2eacec71d8b0b34
Bhavani <bhavanivamshee@gmail.com>
2021-06-30 23:28:59 -0500

    adds HTML to category

--- frontend/javascript/models/category.js
+++ frontend/javascript/models/category.js
@@ -2,6 +2,7 @@ class Category{
     constructor(id, category_name){
         this.id = id
         this.category_name = category_name
+        this.renderCategory()
     }
 
     renderCategory(){
@@ -10,5 +11,13 @@ class Category{
         categoryContainer.dataset.id = this.id
         categoryContainer.id = this.id
         categoryContainer.classList.add = "movie-categories-add"
+        categoryContainer.innerHTML += this.categoryHTML()
+        categoryHolder.appendChild(categoryContainer)
+    }
+
+    categoryHTML(){
+        return `
+        <h2 class="headline">${this.category_name}</h2>
+        `
     }
 }
