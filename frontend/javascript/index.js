1b76c52082e2f2c7176b2ed0fcb72931148ac40d
Bhavani <bhavanivamshee@gmail.com>
2021-07-01 12:00:31 -0500

    figuring out post fetch

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,4 +1,24 @@
+const base_url = "http://localhost:3000/categories"
 document.addEventListener("DOMContentLoaded", function(){
     API.addMoviesCategories()
-})
 
+    constCategoryFormContainer = document.querySelector(".container");
+
+    //fetched data
+
+    const newCategoryForm = document.querySelector(".add-category-form")
+
+        newCategoryForm.addEventListener("submit", event =>{event.preventDefault();
+            const name = event.target.name.value
+            const submit = event.target.submit
+
+            fetch(base_url, {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({})
+            })
+            .then(response => response.json())
+            .then(console.log)
+        })
+
+})
