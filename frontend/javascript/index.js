97e71aa443b93dd4311cfa6a8da8da510ffd194c
Bhavani <bhavanivamshee@gmail.com>
2021-06-30 20:58:03 -0500

    adds categories promise and parse into json promise

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,3 +1,3 @@
 document.addEventListener("DOMContentLoaded", function(){
-    API.addMovies()
+    API.addMoviesCategories()
 })
--- frontend/javascript/services/API.js
+++ frontend/javascript/services/API.js
@@ -1,3 +1,7 @@
 class API {
-    
+    static addMoviesCategories(){
+        fetch("http://localhost:3000/categories")
+        .then(resp => resp.json())
+        .then(console.log)
+    }
 }
